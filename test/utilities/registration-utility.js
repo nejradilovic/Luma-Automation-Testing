03701f6581d2e53173a1447619caf1ef398d352b
Nejra Adilović <nadilovic2@etf.unsa.ba>
2025-01-02 17:55:28 +0100

    Inherit CommonUtility in RegistrationUtility

--- test/utilities/registration-utility.js
+++ test/utilities/registration-utility.js
@@ -1,6 +1,7 @@
 const RegistrationPage = require("../pageobjects/registration-page");
+const CommonUtility = require("./common-utility");
 
-class RegistrationUtility {
+class RegistrationUtility extends CommonUtility {
   async registerUser({ firstName = "", lastName = "", email = "", password = "", confirmPassword = "" } = {}) {
     await RegistrationPage.open();
     await RegistrationPage.inputFirstName.setValue(firstName);
@@ -10,12 +11,13 @@ class RegistrationUtility {
     await RegistrationPage.inputConfirmPassword.setValue(confirmPassword);
     await RegistrationPage.buttonSubmit.click();
   }
-
+  
   async verifySuccessfulRegistration() {
-    const successMessage = await RegistrationPage.registrationSuccessMessage.getText();
-    expect(successMessage).toContain("Thank you for registering with Main Website Store.");
+    const successMessageSelector = RegistrationPage.registrationSuccessMessage;
+    const expectedMessage = "Thank you for registering with Main Website Store.";
+    await this.verifyMessage(successMessageSelector, expectedMessage);
   }
-
+  
   async verifyRequiredFieldErrors() {
     const requiredErrorMessage = "This is a required field.";
 
@@ -29,34 +31,29 @@ class RegistrationUtility {
 
     for (const { field } of requiredFields) {
       if (await field.isDisplayed()) {
-        await this.verifyErrorMessage(field, requiredErrorMessage);
+        await this.verifyMessage(field, requiredErrorMessage);
       }
     }
   }
 
   async verifyInvalidEmailError() {
-    await this.verifyErrorMessage(RegistrationPage.emailErrorMessage, "Please enter a valid email address (Ex: johndoe@domain.com).");
+    await this.verifyMessage(RegistrationPage.emailErrorMessage, "Please enter a valid email address (Ex: johndoe@domain.com).");
   }
 
   async verifyPasswordLengthError() {
-    await this.verifyErrorMessage(RegistrationPage.passwordErrorMessage, "Minimum length of this field must be equal or greater than 8 symbols.");
+    await this.verifyMessage(RegistrationPage.passwordErrorMessage, "Minimum length of this field must be equal or greater than 8 symbols.");
   }
 
   async verifyPasswordCharacterClassError() {
-    await this.verifyErrorMessage(RegistrationPage.passwordErrorMessage, "Minimum of different classes of characters in password is 3.");
+    await this.verifyMessage(RegistrationPage.passwordErrorMessage, "Minimum of different classes of characters in password is 3.");
   }
 
   async verifyPasswordMismatchError() {
-    await this.verifyErrorMessage(RegistrationPage.passwordConfirmationErrorMessage, "Please enter the same value again.");
+    await this.verifyMessage(RegistrationPage.passwordConfirmationErrorMessage, "Please enter the same value again.");
   }
 
   async verifyExistingEmailError() {
-    await this.verifyErrorMessage(RegistrationPage.registrationErrorMessage, "There is already an account with this email address.");
-  }
-
-  async verifyErrorMessage(messageSelector, expectedMessage) {
-    const message = await messageSelector.getText();
-    expect(message).toContain(expectedMessage);
+    await this.verifyMessage(RegistrationPage.registrationErrorMessage, "There is already an account with this email address.");
   }
 }
 
