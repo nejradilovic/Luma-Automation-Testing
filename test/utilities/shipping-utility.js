1573399f136254edd468c8dcbc917982941ce010
Nejra Adilović <nadilovic2@etf.unsa.ba>
2025-01-18 15:50:53 +0100

    Update

--- test/utilities/shipping-utility.js
+++ test/utilities/shipping-utility.js
@@ -2,15 +2,12 @@ const ShippingPage = require("../pageobjects/shipping-page");
 const PaymentsPage = require("../pageobjects/payments-page");
 const SuccessPage = require("../pageobjects/success-page");
 const CommonUtility = require("./common-utility");
-const { is } = require("css-select");
 
 class ShippingUtility extends CommonUtility {
   async completePurchase(streetAddress, city, postalCode, phoneNumber, country) {
     await ShippingPage.fillShippingForm(streetAddress, city, postalCode, phoneNumber, country);
     await PaymentsPage.placeOrder();
-    await SuccessPage.continueShoppingButton.isExisting();
-    const isDisplayed = await SuccessPage.continueShoppingButton.isDisplayed();
-    if(isDisplayed) await SuccessPage.continueShoppingButton.waitForClickable();
+    await SuccessPage.continueShoppingButton.waitForClickable();
     
     const confirmationMessageSelector = SuccessPage.spanThankYouMessage;
     const expectedMessage = "Thank you for your purchase!";
