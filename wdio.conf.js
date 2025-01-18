8de4c9c55e4b29e59c81bbd966dabc0d6a8e64ea
Nejra Adilović <nadilovic2@etf.unsa.ba>
2025-01-18 17:07:01 +0100

    Update

--- wdio.conf.js
+++ wdio.conf.js
@@ -55,11 +55,12 @@ exports.config = {
   // Sauce Labs platform configurator - a great tool to configure your capabilities:
   // https://saucelabs.com/platform/platform-configurator
   //
-  capabilities: [
-    {
-      browserName: "chrome",
-    },
-  ],
+  capabilities: [{
+    browserName: 'chrome',
+    'goog:chromeOptions': {
+        args: ['--start-maximized']  // Ovo može pomoći da se test izvodi u normalnom režimu sa grafičkim interfejsom
+    }
+}],
 
   //
   // ===================
